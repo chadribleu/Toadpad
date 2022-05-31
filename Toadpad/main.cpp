d5c94e30da99deb4891a832f1dc4400bbedb507c
Chadribleu <AdriAcnl456@gmail.com>
2022-05-31 19:08:56 +0200

    Removed duplicated files

--- Toadpad/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "MainWindow.h"
-
-int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPWSTR pCmdLine, int nCmdShow) 
-{
-	MainWindow mainWindow(hInst, nCmdShow);
-
-	return mainWindow.Exec();
-}
